a271f7ea8584f22fd21b59a69f8b4773ef7844fe
Simon <S.platiotis@gmail.com>
2021-04-15 16:01:43 +0300

    Initialize repo

---
+++ src/main/java/ihu/dypa/emporium/EmporiumApplication.java
@@ -0,0 +1,13 @@
+package ihu.dypa.emporium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmporiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmporiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ihu/dypa/emporium/EmporiumApplicationTests.java
@@ -0,0 +1,13 @@
+package ihu.dypa.emporium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmporiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
