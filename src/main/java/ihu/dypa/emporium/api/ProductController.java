90b3abac99e21b8f457a10f42aee0096bedc2405
Christos Avramis <33569533+christosavramis@users.noreply.github.com>
2021-05-18 03:48:38 +0300

    fixxed some typos and added buy list page

--- src/main/java/ihu/dypa/emporium/api/ProductController.java
+++ src/main/java/ihu/dypa/emporium/api/ProductController.java
@@ -5,11 +5,9 @@ import ihu.dypa.emporium.service.ProductService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController @RequestMapping("/api/products")
 @RequiredArgsConstructor(onConstructor=@__({@Autowired}))
 public class ProductController {
--- src/main/java/ihu/dypa/emporium/api/RetailerController.java
+++ src/main/java/ihu/dypa/emporium/api/RetailerController.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("unchecked")
+@CrossOrigin
 @RestController @RequestMapping("/api/retailers")
 @RequiredArgsConstructor(onConstructor=@__({@Autowired}))
 public class RetailerController {
